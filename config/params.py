dcf45c387017979b9ec13dacf1f6468f449b815e
tetrix <porjazd1@login2.triton.aalto.fi>
2019-09-26 11:41:20 +0300

    changed params

--- config/params.py
+++ config/params.py
@@ -3,17 +3,20 @@ word_hidden_size = 300
 word_num_layers = 4
 
 char_embedding_dim = 100
-char_hidden_size = 100
-char_num_layers = 4
+char_hidden_size = 75
+char_num_layers = 2
 
 morph_embedding_dim = 100
-morph_hidden_size = 100
-morph_num_layers = 4
+morph_hidden_size = 75
+morph_num_layers = 2
 
-batch_size = 64
-num_epochs = 50
+batch_size = 128
+num_epochs = 40
 dropout_prob = 0.5
 learning_rate = 0.001
 patience = 10
 MAX_SEQ_LENGTH = 100
 skip_training = False
+
+# highway dropout 0.7, 4 layers
+# no early stopping
