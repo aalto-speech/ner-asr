567bb95020e1fc6580258cb6f86f6a1763e0fbd1
Porjazovski Dejan <porjazd1@t40511-da036.org.aalto.fi>
2020-03-12 11:01:01 +0200

    changed some params

---
+++ config/params.py
@@ -0,0 +1,20 @@
+word_embedding_dim = 300
+word_hidden_size = 300
+word_num_layers = 2
+
+char_embedding_dim = 100
+char_hidden_size = 75
+char_num_layers = 2
+
+morph_embedding_dim = 100
+morph_hidden_size = 75
+morph_num_layers = 2
+
+batch_size = 128
+num_epochs = 75
+dropout_prob = 0.5
+learning_rate = 0.001
+patience = 10
+MAX_SEQ_LENGTH = 1000
+skip_training = True
+lowercase_model = False
